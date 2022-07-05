93277854a20a0dfd388b036798363e2ca22b8015
DanielleCavalcante <108361499+DanielleCavalcante@users.noreply.github.com>
2022-07-05 16:58:21 -0300

    Rename README.mk to README

---
+++ README
@@ -0,0 +1,3 @@
+<h1> Projeto de Recriação - Pagina Inicial do Instagram
+
+Projeto desenvolvido para conclusão do desafio da plataforma DIO.me
--- README.mk
+++
@@ -1,3 +0,0 @@
-<h1> Projeto de Recriação - Pagina Inicial do Instagram
-
-Projeto desenvolvido para conclusão do desafio da plataforma DIO.me
